6a40d857a674d18273ea64159529aee52bfe1e4e
noeliaserrano <noelia.serrano@udc.es>
2023-10-21 15:59:24 +0200

    informe

--- p2.c
+++ p2.c
@@ -5,35 +5,33 @@
 
 int main()
 {
-    init_rand_seed();
-    // test_ord_ins();
-    // test_ord_shell();
+    /*init_rand_seed();
+    test_ord_ins();
+    test_ord_shell();
+    
+    printf("\n");
+    printf("INSERTION SORT\n");
+    time_ord_ins_rand();
+    time_ord_ins_asc();
+    time_ord_ins_desc();
 
-    // printf("INSERTION SORT\n");
-    // time_ord_ins_rand();
-    // time_ord_ins_asc();
-    // time_ord_ins_desc();
-
-    // printf("\n");
-
-    // printf("INSERTION SHELL\n");
+    printf("\n");
+*/
+    printf("INSERTION SHELL\n");
     time_ord_shell_rand();
     time_ord_shell_asc();
     time_ord_shell_desc();
 }
 
 // Algoritmo 1
-void ord_ins(int v[], int n)
-{ // Mejor : linear, Peor : n^2, Medio : n^2
+void ord_ins(int v[], int n) { // Mejor : linear, Peor : n^2, Medio : n^2
     int x, j;
 
-    for (int i = 1; i < n; i++)
-    {
+    for (int i = 1; i < n; i++) {
         x = v[i];
         j = i - 1;
 
-        while (j >= 0 && v[j] > x)
-        { // mejor caso const
+        while (j >= 0 && v[j] > x) { // mejor caso const
             v[j + 1] = v[j];
             j = j - 1;
         }
@@ -42,27 +40,22 @@ void ord_ins(int v[], int n)
 }
 
 // Algoritmo 2
-void ord_shell(int v[], int n)
-{
+void ord_shell(int v[], int n) {
     int incremento = n;
     int j, tmp;
     bool seguir;
 
-    do
-    {
+    do {
         incremento = incremento / 2;
 
-        for (int i = incremento; i < n; i++)
-        {
+        for (int i = incremento; i < n; i++) {
             tmp = v[i];
             j = i;
             seguir = true;
 
-            while (j - incremento >= 0 && seguir)
-            { // mejor caso : const
+            while (j - incremento >= 0 && seguir) { // mejor caso : const
 
-                if (tmp < v[j - incremento])
-                {
+                if (tmp < v[j - incremento]) {
                     v[j] = v[j - incremento];
                     j = j - incremento;
                 }
@@ -75,8 +68,7 @@ void ord_shell(int v[], int n)
 }
 
 // Tests //MAYBE move to bootstrap
-void test_ord_ins()
-{
+void test_ord_ins() {
     const int n = 10;
     int v[10] = {0};
 
@@ -102,8 +94,7 @@ void test_ord_ins()
     print_vector_int(v, n);
 }
 
-void test_ord_shell()
-{
+void test_ord_shell() {
     const int n = 10;
     int v[10] = {0};
 
@@ -129,62 +120,50 @@ void test_ord_shell()
     print_vector_int(v, n);
 }
 
-double n_to_1_8(double n)
-{
+double n_to_1_8(double n) {
     return pow(n, 1.8);
 }
-double n_squared(double n)
-{
+double n_squared(double n) {
     return n * n;
 }
-double n_to_2_2(double n)
-{
+double n_to_2_2(double n) {
     return pow(n, 2.2);
 }
-double n_to_1_2(double n)
-{
+double n_to_1_2(double n) {
     return pow(n, 1.2);
 }
-double linear(double n)
-{
+double linear(double n) {
     return n;
 }
-double n_to_0_8(double n)
-{
+double n_to_0_8(double n) {
     return pow(n, 0.8);
 }
-double nlogn(double n)
-{
+double nlogn(double n) {
     return n * log(n);
 }
-double nlogn_to_0_8(double n)
-{
+double nlogn_to_0_8(double n) {
     return pow(n * log(n), 0.8);
 }
-double nlogn_to_1_2(double n)
-{
+double nlogn_to_1_2(double n) {
     return pow(n * log(n), 1.2);
 }
 
 // ORD_INS
-void time_ord_ins_rand()
-{
+void time_ord_ins_rand() {
     printf("************ ALEATORIO ************\n");
     MEASURE_TIME_TABLE(aleatorio, ord_ins,
                        "n^1.8", "n^2", "n^2.2",
                        n_to_1_8, n_squared, n_to_2_2);
 }
 
-void time_ord_ins_asc()
-{
+void time_ord_ins_asc() {
     printf("************ ASCENDENTE ************\n");
     MEASURE_TIME_TABLE(ascencente, ord_ins,
                        "n^0.8", "n", "n^1.2",
                        n_to_0_8, linear, n_to_1_2);
 }
 
-void time_ord_ins_desc()
-{
+void time_ord_ins_desc() {
     printf("************ DESCENDENTE ************\n");
     MEASURE_TIME_TABLE(descendente, ord_ins,
                        "n^1.8", "n^2", "n^2.2",
@@ -193,16 +172,14 @@ void time_ord_ins_desc()
 
 // ORD_SHELL
 // No es el peor caso, el while mas interno de media se va ejecutar varias veces
-void time_ord_shell_rand()
-{
+void time_ord_shell_rand() {
     printf("************ ALEATORIO ************\n");
     MEASURE_TIME_TABLE(aleatorio, ord_shell,
                        "(n*log n)^0.8", "n*log n", "(n*log n)^1.2",
                        nlogn_to_0_8, nlogn, nlogn_to_1_2);
 }
 // Es el mejor caso, el while mas interno no se ejecuta
-void time_ord_shell_asc()
-{
+void time_ord_shell_asc() {
     printf("************ ASCENDENTE ************\n");
     MEASURE_TIME_TABLE(ascencente, ord_shell,
                        "(n*log n)^0.8", "n*log n", "(n*log n)^1.2",
@@ -210,8 +187,7 @@ void time_ord_shell_asc()
 }
 // Descendente no es el peor caso, de hecho es relativamente bueno
 // Por que el while mas interno solo se ejecuta 1 vez
-void time_ord_shell_desc()
-{
+void time_ord_shell_desc() {
     printf("************ DESCENDENTE ************\n");
     MEASURE_TIME_TABLE(descendente, ord_shell,
                        "(n*log n)^0.8", "n*log n", "(n*log n)^1.2",
