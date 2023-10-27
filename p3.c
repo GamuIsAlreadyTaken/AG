ddb58718eb864e685a02f57e1000fd3209349e32
IsmaelMiguez <i.miguezv@udc.es>
2023-10-27 15:50:14 +0200

    p3 creacion archivos

---
+++ p3.c
@@ -0,0 +1,5 @@
+//
+// Created by ismael on 27/10/23.
+//
+
+#include "p3.h"
---
+++ p3.h
@@ -0,0 +1,8 @@
+//
+// Created by ismael on 27/10/23.
+//
+
+#ifndef PRACTICAS_P3_H
+#define PRACTICAS_P3_H
+
+#endif //PRACTICAS_P3_H
