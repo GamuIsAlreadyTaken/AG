2bffd9ff8da38feddd4ae6104d41efa236296a44
IsmaelMiguez <i.miguezv@udc.es>
2023-10-31 10:48:44 +0100

    p3

--- p3.c
+++ p3.c
@@ -3,3 +3,59 @@
 //
 
 #include "p3.h"
+#include "bootstrap.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void inicializarMonticulo (pmonticulo m){
+    m->ultimo = NULL;
+}
+
+void crearMonticulo(int v [], int n, pmonticulo m) {
+    if (n>TAM){
+        printf("Vector demasiado grande");
+        return;
+    }
+    m->ultimo = n;
+    for (int i = 0; i < n; i++) {
+        m->vector[i] = v[i];
+    }
+}
+
+int Padre(pmonticulo m, int i) {
+    return i/2;
+}
+int Izquierdo(pmonticulo m, int i) {
+    return 2*i;
+}
+int Derecho(pmonticulo m, int i) {
+    return 2*i+1;
+}
+
+
+
+
+int quitarMenor(pmonticulo m){
+
+}
+
+
+//Algoritmo
+void OrdenarPorMonticulos(int v[], int a){
+
+    crearMonticulo(v ,a ,m);
+    for (int i = 0; i < n; i++) {
+        V[i] = quitarMenor(M);
+    }
+}
+
+
+int main () {
+    inicializar_semilla();
+    int v[];
+    int n = sizeof(v) / sizeof(v[0]);
+    pmonticulo m = (pmonticulo)malloc(sizeof(struct monticulo));
+
+
+
+}
--- p3.h
+++ p3.h
@@ -6,3 +6,22 @@
 #define PRACTICAS_P3_H
 
 #endif //PRACTICAS_P3_H
+#define TAM 256000
+#define NULL NULL
+
+
+struct monticulo {
+    int ultimo;
+    int vector[TAM];
+};
+typedef struct monticulo * pmonticulo;
+
+void inicializarMonticulo (pmonticulo m);
+void crearMonticulo(int vector[], int n, pmonticulo m);
+int quitarMenor(pmonticulo m);
+
+
+//Funciones auxiliares
+int Padre(pmonticulo m, int i);
+int Izquierdo(pmonticulo m, int i);
+int Derecho(pmonticulo m, int i);
