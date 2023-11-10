6e84135e36f6a077e87322a4d63a9b2ad805716c
GamuIsAlreadyTaken <52568687+GamuIsAlreadyTaken@users.noreply.github.com>
2023-11-10 15:16:56 +0000

    cambios

--- p3.c
+++ p3.c
@@ -15,18 +15,23 @@ int main()
     printf("HEAP SORT\n");
     time_crearMonticulo();
 }
-
+// Peor caso -> n log n
+// Caso medio-> n log n
+// Mejor caso-> n
 void crearMonticulo(int v[], int n, pmonticulo m)
 {
     // Copiar el vector en el struct del monticulo
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) // n
         m->vector[i] = v[i];
     m->ultimo = n;
 
-    for (int i = m->ultimo / 2; i >= 0; i--)
+    for (int i = m->ultimo / 2; i >= 0; i--) // (n/2) * hundir -> n * hundir
         hundir(m, i);
 }
 
+// Peor caso -> hundir hasta el final -> log n
+// Caso medio-> hundir a la mitad -> log (n/2) -> log n
+// Mejor caso-> no se hunde nada -> 1
 void hundir(pmonticulo m, int i)
 {
     int j, hijoIzq, hijoDer, tmp;
@@ -41,6 +46,7 @@ void hundir(pmonticulo m, int i)
 
         if (hijoIzq < m->ultimo && m->vector[hijoIzq] < m->vector[i])
             i = hijoIzq;
+
         // Intercambiar m->vector[j] y m->vector[i]
         tmp = m->vector[j];
         m->vector[j] = m->vector[i];
@@ -56,19 +62,22 @@ void listar_monticulo(pmonticulo m)
     printf("]\n");
 }
 
+// Peor caso -> log n
+// Caso medio-> log (n/2) -> log n
+// Mejor caso-> 1
 int quitarMenor(pmonticulo m)
 {
     int x;
     if (monticuloVacio(m))
-        assert(0 && "Monticulo vacio");
+        assert(0 && "Monticulo vacío");
     x = m->vector[0];
     m->vector[0] = m->vector[m->ultimo - 1];
     m->ultimo -= 1;
-    if (!monticuloVacio(m))
+    if (!monticuloVacio(m)) // hundir
         hundir(m, 0);
     return x;
 }
-
+// Todos casos -> 1
 bool monticuloVacio(pmonticulo m)
 {
     return m->ultimo == 0;
@@ -97,16 +106,16 @@ void test_ordenarPorMonticulos()
     printf("]\n");
 }
 
-
-void OrdenarPorMonticulo (int v[], int n){
+void OrdenarPorMonticulo(int v[], int n)
+{
     struct monticulo m;
-    int i ;
+    int i;
     crearMonticulo(v, n, &m);
-    for (i = 0; i < n-1; i++) {
-        V[i] = quitarMenor(M)
+    for (i = 0; i < n - 1; i++)
+    {
+        v[i] = quitarMenor(&m);
     }
 }
-
 
 double n_to_1_2(double n)
 {
@@ -120,11 +129,36 @@ double n_to_0_8(double n)
 {
     return pow(n, 0.8);
 }
+
 void time_crearMonticulo()
 {
     struct monticulo m;
     MEASURE_TIME_TABLE(aleatorio, crearMonticulo(v, n, &m),
                        "n^0.8", "n", "n^1.2",
                        n_to_0_8, linear, n_to_1_2);
+}
 
+void time_ordenarPorMonticulos_rand()
+{
+    struct monticulo m;
+    MEASURE_TIME_TABLE(aleatorio, crearMonticulo(v, n, &m),
+                       "n^0.8", "n", "n^1.2",
+                       n_to_0_8, linear, n_to_1_2);
+    // Por determinar complejidad
+}
+void time_ordenarPorMonticulos_asc()
+{
+    struct monticulo m;
+    MEASURE_TIME_TABLE(ascendente, crearMonticulo(v, n, &m),
+                       "n^0.8", "n", "n^1.2",
+                       n_to_0_8, linear, n_to_1_2);
+    // Por determinar complejidad
+}
+void time_ordenarPorMonticulos_desc()
+{
+    struct monticulo m;
+    MEASURE_TIME_TABLE(descendente, crearMonticulo(v, n, &m),
+                       "n^0.8", "n", "n^1.2",
+                       n_to_0_8, linear, n_to_1_2);
+    // Por determinar complejidad
 }
