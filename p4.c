1548cfa95d0ba493b4b64f4ef24e2264513a9602
IsmaelMiguez <i.miguezv@udc.es>
2023-11-17 15:55:58 +0100

    Init p4

---
+++ p4.c
@@ -0,0 +1,5 @@
+//
+// Created by ismael on 17/11/23.
+//
+
+#include "p4.h"
---
+++ p4.h
@@ -0,0 +1,8 @@
+//
+// Created by ismael on 17/11/23.
+//
+
+#ifndef PRACTICAS_P4_H
+#define PRACTICAS_P4_H
+
+#endif //PRACTICAS_P4_H
