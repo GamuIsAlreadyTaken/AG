7783b4dae79b27e81006ad1ab28b1f9e5d14d6d3
noeliaserrano <noelia.serrano@udc.es>
2024-07-02 19:20:57 +0200

    revisa porfa

--- examNoe/exam.c
+++ examNoe/exam.c
@@ -3,18 +3,6 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-/* Ordenar ascendentemente un vector con el siguiente algoritmo
-Me da asi los vectores
-	Test ordenado
-Aleatorio:  [ -2 -2  3 -8 -4 -2  7 -9  9 -9 ]
-Ordenado:   [ -2 -2  3 -8 -4 -2  7 -9  9 -1573584821 ]
-
-Ascendente: [  0  1  2  3  4  5  6  7  8  9 ]
-Ordenado:   [  0  1  2  3  4  5  6  7  8 -9 ]
-
-Descendente:    [  10  9  8  7  6  5  4  3  2  1 ]
-Ordenado:   [  10  9  8  7  6  5  4  3  2  1 ]
-*/
 
 void ordenar(int v[], int n)
 { 
@@ -24,28 +12,29 @@ void ordenar(int v[], int n)
     int j = n - 1;
 
 
-    while (i > 0){ 
+    while (i < j){ 
     	x = v[i];
     	v[i] = v[j];
     	v[j] = x;
 
     	i = i + 1;
-    	j = j -1;
+    	j = j - 1;
 
     };
 
-    for (i = n-1; i < n; i++) //para i = n - 1 hasta 1 paso -1 hacer
+    for (i = n - 1; i > 0; i--) 
     {
     	x = v[i];
     	j = i + 1;
 
-    	while(j <= n && v[j] < x){ //mientras j <= n y v[j] < x hacer
+    	while (j <= n && v[j-1] < x) {
     		v[j-1] = v[j];
     		j = j + 1;
     	};
     	v[j-1] = x;
     }
 }
+
 
 double n_to_1_8(double n)
 {
@@ -139,7 +128,7 @@ int main()
 
     printf("\n");
 
-    /*printf("ORDENAR\n");
+    printf("ORDENAR\n");
 
     printf("************ ALEATORIO ************\n");
     MEASURE_TIME_TABLE(aleatorio, ordenar(v, n),
@@ -155,7 +144,6 @@ int main()
     MEASURE_TIME_TABLE(descendente, ordenar(v, n),
                        "(n*log n)^0.8", "n*log n", "(n*log n)^1.25",
                        nlogn_to_0_8, nlogn, nlogn_to_1_25);
-    */
 
     printf("\n");
 
